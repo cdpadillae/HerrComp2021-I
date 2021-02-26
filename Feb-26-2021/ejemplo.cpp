ea45497b94896d25c5054932ba5de5a26086a93d
cdpadillae <cdpadillae@unal.edu.co>
2021-02-26 12:02:40 -0500

    Hello World Herr Compt Clase

---
+++ Feb-26-2021/ejemplo.cpp
@@ -0,0 +1,8 @@
+#include <iostream> //Para que pueda imprimir info a la pantalla
+
+int main(int argc, char **argv) //Siempre debe existir int main.
+{
+  std::cout << "Hola Mundo\n";
+
+  return 0;
+}
