b4326e8fe62464b4448ea3641bdf00a325cce1c5
cdpadillae <cdpadillae@unal.edu.co>
2021-02-26 12:40:33 -0500

    Linea con entrada q multiplica x2

--- Feb-26-2021/ejemplo.cpp
+++ Feb-26-2021/ejemplo.cpp
@@ -1,8 +1,13 @@
 #include <iostream> //Para que pueda imprimir info a la pantalla
+#include <cstdlib>
 
 int main(int argc, char **argv) //Siempre debe existir int main.
 {
+  int a = std::atoi(argv[1]);
+  a = 2*a;
+
   std::cout << "Hola Mundo, soy yo de nuevo jajaja\n";
+  std::cout << "2*a:" << a << std::endl;
 
   return 0;
 }
